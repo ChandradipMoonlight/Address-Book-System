f720fe9e03d34d2ca4fa7af9ca171c40c8e0ee16
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-18 17:24:51 +0530

    Greeting message added

---
+++ AddressBookSystem.java
@@ -0,0 +1,10 @@
+package addressbooksystem;
+
+public class AddressBookSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the address book program ");
+
+	}
+
+}
